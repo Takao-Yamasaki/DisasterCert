c37d2de204742bfa27d5e4a62d2436a4c9ce6e58
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-02 10:04:34 +0900

    住所のメッセージ追加

--- server.js
+++ server.js
@@ -45,6 +45,18 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
         }
     });
 
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+            events_processed.push(bot.replyMessage(event.replyToken, {
+                type: "text",
+                text: "あなたの住所を入力してください。"
+            }));
+        }
+    });
+
+
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
     Promise.all(events_processed).then(
         (response) => {
