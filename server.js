82561eb14334c322c28fb6dbd9fafd24a277db66
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-03 20:00:59 +0900

    Improve

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "氏名を入力してください"
+                    text: "${event.source.userId}"
                 }));
             } else {
                 events_processed.push(bot.replyMessage(event.replyToken, {
