35441b0d9816f29af6326a3097b35c9de2fce0cf
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-03 19:53:28 +0900

    Improve

--- server.js
+++ server.js
@@ -28,16 +28,17 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // すべてのイベント処理のプロセスを格納する配列。
     let events_processed = [];
 
+    let userId = event.events.source.userId;
+
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
             if (event.message.text == "こんにちは"){
-                let userId = event.events.source.userId;
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: userId
+                    text: "${userId}"
                 }));
             } else {
                 events_processed.push(bot.replyMessage(event.replyToken, {
