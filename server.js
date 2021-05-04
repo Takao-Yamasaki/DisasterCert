98683ee6b8aec82927c93b753d70315a543453fd
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-04 21:51:00 +0900

    Improve

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
         if (event.type == "message" && event.message.type == "text"){
             userRef.child(userId).on('value',function(snapshot){
                 var userData = snapshot.val();
-                if (userData['stage'] == null) {
+                if (userData == null) {
                     userRef.child(userId).set({
                         stage: 0
                     });
