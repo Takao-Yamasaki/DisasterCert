56d75cad10b46901951f16fc7dde9756202e5540
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-04 21:33:58 +0900

    Improve

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
             userRef.child(userId).on('value',function(snapshot){
                 var userData = snapshot.val();
                 switch (userData['stage']) {
-                    default:
+                    case null:
                         // replyMessage()で返信し、そのプロセスをevents_processedに追加。
                         events_processed.push(bot.replyMessage(event.replyToken, [{
                             type: "text",
