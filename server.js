b795e69dec9dffc1a2ff52d7c60fd803d36e1f73
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-05 09:10:16 +0900

    Improve

--- server.js
+++ server.js
@@ -55,11 +55,11 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
         if (event.type == "message" && event.message.type == "text"){
             userRef.child(userId).on('value',function(snapshot){
                 var userData = snapshot.val();
-                // if (userData == null) {
-                //     userRef.child(userId).set({
-                //         stage: 1
-                //     });
-                // }
+                if (snapshot.exists() == null) {
+                    userRef.child(userId).set({
+                        stage: 1
+                    });
+                }
                 switch (userData['stage']) {
                     case 1:
                         // replyMessage()で返信し、そのプロセスをevents_processedに追加。
@@ -82,10 +82,10 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                             text: "あなたの「住所」を入力してください" + userData['stage'] 
                         }));
                         // storage.userId.stage = 2;
-                        userRef.child(userId).set({
-                            stage: 2,
-                            name: events.message.text
-                        });
+                        // userRef.child(userId).set({
+                        //     stage: 2,
+                        //     name: events.message.text
+                        // });
                         break;
                 }
             });
