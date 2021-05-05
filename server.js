fe9da7ecab45d57fc45b4da13307e066f533aef0
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-05 11:28:46 +0900

    Improve

--- server.js
+++ server.js
@@ -71,17 +71,17 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                         stage: 1
                     });
                 // };
-                // switch (userData['stage']) {
-                //     case 1: 
-                        // events_processed.push(bot.replyMessage(event.replyToken, {
-                        //     type: "text",
-                        //     text: "あなたの「住所」を入力してください" + userData['stage'] 
-                        // }));
-                        // userRef.child(userId).set({
-                        //     stage: 2,
-                        // });
-                        // break;
-                // }
+                switch (userData['stage']) {
+                    case 1: 
+                        events_processed.push(bot.replyMessage(event.replyToken, {
+                            type: "text",
+                            text: "あなたの「住所」を入力してください" + userData['stage'] 
+                        }));
+                        userRef.child(userId).set({
+                            stage: 2,
+                        });
+                        break;
+                }
             });
         }
     }); 
