9cca45f21fc869d33bffb2330ae4f2e6fb345c01
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-05 09:41:22 +0900

    Improve

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                         },
                         {
                             type: "text",
-                            text: "あなたの「氏名」を入力してください"  + userData['stage']
+                            text: "あなたの「氏名」を入力してください"
                         }])); 
                         storage.userId.stage = 1;
                         userRef.child(userId).set({
