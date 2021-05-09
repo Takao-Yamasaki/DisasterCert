12eaa45dd004581886f3afb97bb1146e90bd9d64
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-05-09 20:06:50 +0900

    Improve

--- server.js
+++ server.js
@@ -206,8 +206,8 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                     break;
                 case 8:
                     userRef.child(userId).update({
-                        stage: userData['stage'] + 1
-                        // pic: userImg
+                        stage: userData['stage'] + 1,
+                        pic: userImg
                     });
                     break;
                 case 9:
